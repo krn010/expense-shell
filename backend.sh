fcaf29e6648d85537e8fc49a3d8a5d62cabbd321
Karan <metalheadgmail.com>
2024-06-20 20:46:05 +0530

    Modified mysql configuration in backend

--- backend.sh
+++ backend.sh
@@ -1,45 +1,45 @@
-echo "Disable NodeJS Default Version"
+echo -e "\e[32mDisable NodeJS Default Version\e[0m"
 dnf module disable nodejs -y
 
-echo "Enable NodeJS 18 Version"
+echo -e "\e[32mEnable NodeJS 18 Version\e[0m"
 dnf module enable nodejs:18 -y
 
-echo "Install NodeJS"
+echo -e "\e[32mInstall NodeJS\e[0m"
 dnf install nodejs -y
 
-echo "Configure Backend Service"
+echo -e "\e[32mConfigure Backend Service\e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
-echo "Adding Application User"
+echo -e "\e[32mAdding Application User\e[0m\e[0m"
 useradd expense
 
-echo "Remove existing App content"
+echo -e "\e[32mRemove existing App content\e[0m"
 rm -rf /app
 
-echo "Create Application Directory"
+echo -e "\e[32mCreate Application Directory\e[0m"
 mkdir /app
 
-echo "Download Application Content"
+echo -e "\e[32mDownload Application Content\e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 
-echo "Extracting Application Content"
+echo -e "\e[32mExtracting Application Content\e[0m"
 unzip /tmp/backend.zip
 
-echo "Downloading Application Dependencies"
+echo -e "\e[32mDownloading Application Dependencies\e[0m"
 npm install
 
 
-echo "Reloading SystemD and Starting Backend Service"
+echo -e "\e[32mReloading SystemD and Starting Backend Service\e[0m"
 systemctl daemon-reload
 
 systemctl enable backend
 systemctl restart backend
 
-echo "Install MySQL Client"
+echo -e "\e[32mInstall MySQL Client\e[0m"
 dnf install mysql -y
 
-echo "Load Schema"
+echo -e "\e[32mLoad Schema\e[0m"
 mysql -h 172.31.41.227 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
