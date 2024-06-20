0a158e3e4a781901acb81f085877200912fd7bad
Karan <metalheadgmail.com>
2024-06-20 21:47:43 +0530

    Added variable password in mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,4 @@
+MYSQL_PASSWORD=$1
 dnf module disable mysql -y
 
 cp mysql.repo /etc/yum.repos.d/mysql.repo
@@ -10,7 +11,7 @@ systemctl start mysqld
 
 
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ${MYSQL_PASSWORD}
 
 
 
