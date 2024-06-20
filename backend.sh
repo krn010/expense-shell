5d1437984fb3675e2de05941d5dffd411611ec4d
Karan <metalheadgmail.com>
2024-06-20 21:09:16 +0530

    Modified mysql configuration in backend

--- backend.sh
+++ backend.sh
@@ -1,46 +1,46 @@
 echo -e "\e[32mDisable NodeJS Default Version\e[0m"
-dnf module disable nodejs -y
+dnf module disable nodejs -y >>/tmp/expense.log
 
 echo -e "\e[32mEnable NodeJS 18 Version\e[0m"
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y >>/tmp/expense.log
 
 echo -e "\e[32mInstall NodeJS\e[0m"
 dnf install nodejs -y
 
 echo -e "\e[32mConfigure Backend Service\e[0m"
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service >>/tmp/expense.log
 
 echo -e "\e[32mAdding Application User\e[0m\e[0m"
-useradd expense
+useradd expense >>/tmp/expense.log
 
 echo -e "\e[32mRemove existing App content\e[0m"
-rm -rf /app
+rm -rf /app >>/tmp/expense.log
 
 echo -e "\e[32mCreate Application Directory\e[0m"
-mkdir /app
+mkdir /app >>/tmp/expense.log
 
 echo -e "\e[32mDownload Application Content\e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>/tmp/expense.log
 cd /app
 
 echo -e "\e[32mExtracting Application Content\e[0m"
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip >>/tmp/expense.log
 
 echo -e "\e[32mDownloading Application Dependencies\e[0m"
-npm install
+npm install >>/tmp/expense.log
 
 
 echo -e "\e[32mReloading SystemD and Starting Backend Service\e[0m"
-systemctl daemon-reload
+systemctl daemon-reload >>/tmp/expense.log
 
-systemctl enable backend
-systemctl restart backend
+systemctl enable backend >>/tmp/expense.log
+systemctl restart backend >>/tmp/expense.log
 
 echo -e "\e[32mInstall MySQL Client\e[0m"
-dnf install mysql -y
+dnf install mysql -y >>/tmp/expense.log
 
 echo -e "\e[32mLoad Schema\e[0m"
-mysql -h 172.31.41.227 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.41.227 -uroot -pExpenseApp@1 < /app/schema/backend.sql >>/tmp/expense.log
 
 
 
