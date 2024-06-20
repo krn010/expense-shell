2c306eb9c5dff4dfb3aad4bb067a771d26afb4c1
Karan <metalheadgmail.com>
2024-06-20 22:28:18 +0530

    Added variable password in mysql.sh

--- backend.sh
+++ backend.sh
@@ -1,48 +1,52 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
 
-echo -e "\e[32mDisable NodeJS Default Version\e[0m"
+Head () {
+  echo -e "\e[35m$1\e[om"
+}
+
+Head "Disable NodeJS Default Version"
 dnf module disable nodejs -y &>>${log_file}
 
-echo -e "\e[32mEnable NodeJS 18 Version\e[0m"
+Head "Enable NodeJS 18 Version"
 dnf module enable nodejs:18 -y &>>${log_file}
 
-echo -e "\e[32mInstall NodeJS\e[0m"
+Head "Install NodeJS"
 dnf install nodejs -y &>>/tmp/expe
 
-echo -e "\e[32mConfigure Backend Service\e[0m"
+Head "Configure Backend Service"
 cp backend.service /etc/systemd/system/backend.service &>>${log_file}
 
-echo -e "\e[32mAdding Application User\e[0m\e[0m"
+Head "Adding Application User"
 useradd expense &>>${log_file}
 
-echo -e "\e[32mRemove existing App content\e[0m"
+Head "Remove existing App content"
 rm -rf /app &>>${log_file}
 
-echo -e "\e[32mCreate Application Directory\e[0m"
+Head "Create Application Directory"
 mkdir /app &>>${log_file}
 
-echo -e "\e[32mDownloading Application Content\e[0m"
+Head "Downloading Application Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>${log_file}
 cd /app
 
-echo -e "\e[32mExtracting Application Content\e[0m"
+Head "Extracting Application Content"
 unzip /tmp/backend.zip &>>${log_file}
 
-echo -e "\e[32mDownloading Application Dependencies\e[0m"
+Head "Downloading Application Dependencies"
 npm install &>>${log_file}
 
 
-echo -e "\e[32mReloading SystemD and Starting Backend Service\e[0m"
+Head "Reloading SystemD and Starting Backend Service"
 systemctl daemon-reload &>>${log_file}
 
 systemctl enable backend &>>${log_file}
 systemctl restart backend &>>${log_file}
 
-echo -e "\e[32mInstall MySQL Client\e[0m"
+Head "Install MySQL Client"
 dnf install mysql -y &>>${log_file}
 
-echo -e "\e[32mLoad Schema\e[0m"
+Head "Load Schema"
 mysql -h 172.31.41.227 -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>${log_file}
 
 
