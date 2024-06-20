8466b91026d11f66b3126ad365aebe437710901c
Karan <metalheadgmail.com>
2024-06-20 20:27:20 +0530

    Modified mysql configuration in backend

--- backend.sh
+++ backend.sh
@@ -1,32 +1,45 @@
+echo "Disable NodeJS Default Version"
 dnf module disable nodejs -y
+
+echo "Enable NodeJS 18 Version"
 dnf module enable nodejs:18 -y
 
+echo "Install NodeJS"
 dnf install nodejs -y
 
+echo "Configure Backend Service"
 cp backend.service /etc/systemd/system/backend.service
 
+echo "Adding Application User"
 useradd expense
 
+echo "Remove existing App content"
 rm -rf /app
 
+echo "Create Application Directory"
 mkdir /app
 
+echo "Download Application Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
+
+echo "Extracting Application Content"
 unzip /tmp/backend.zip
 
-cd /app
+echo "Downloading Application Dependencies"
 npm install
 
 
-
+echo "Reloading SystemD and Starting Backend Service"
 systemctl daemon-reload
 
 systemctl enable backend
 systemctl restart backend
 
+echo "Install MySQL Client"
 dnf install mysql -y
 
+echo "Load Schema"
 mysql -h 172.31.41.227 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
