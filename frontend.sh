3628648b7c8fb65a2b7c83ac851d1f663fb35a9f
Karan <metalheadgmail.com>
2024-06-21 20:29:10 +0530

    changeds in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -3,11 +3,11 @@ source common.sh
 
 Head "Install Nginx"
 dnf install nginx -y &>>${log_file}
-echo $?
+Stat $?
 
 Head "Copy Expense config file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>${log_file}
-echo $?
+Stat $?
 
 
 
@@ -16,4 +16,4 @@ App_Prereq "/usr/share/nginx/html"
 Head "Start Nginx Service"
 systemctl enable nginx &>>${log_file}
 systemctl restart nginx &>>${log_file}
-echo $?
+Stat $?
